4945c833f1189f07bf6c42bb75bf7a0733639f6b
benyoo5222 <benyoo5222@gmail.com>
2018-04-17 16:21:43 +0000

    the express_server.js now renders the index page in views that sends the browser url and shortened form as well as a link to shorten urls

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+app.set("view engine", "ejs"); // telling the express library to run ejs(embedded javascript) templates to run in a folder called views
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -9,6 +11,11 @@ var urlDatabase = {
 
 app.get("/", (req, res) => { //handles any get method with the url of /
   res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => { //handles any get method to url of /urls and renders the urls_index page & the object of the urldatabase
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => { //handles any get method with the url of /urls.json
