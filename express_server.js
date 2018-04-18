aca78ac2d123d989963152a5214bb20106db920b
benyoo5222 <benyoo5222@gmail.com>
2018-04-18 19:42:36 +0000

    learned how to branch off and then merge. Learned how to pass cookie name and values and used request body to see if cookies value were defined and depending on yes or no, the header changed to reflect either login or logout

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+var cookieParser = require('cookie-parser'); // using the cookieparser
+
 
 const bodyParser = require("body-parser");
-
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -14,23 +16,39 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => { //handles any get method with the url of /
+/*app.get("/", (req, res) => { //handles any get method with the url of /
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
+});*/
 
 app.get("/urls", (req, res) => { //handles any get method to url of /urls and renders the urls_index page & the object of the urldatabase
-  let templateVars = { urls: urlDatabase };
-  console.log(req.headers);
+  let templateVars = { urls: urlDatabase, username: req.cookies['username']};
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls.json", (req, res) => { //handles any get method with the url of /urls.json
-  res.json(urlDatabase);
-});
 
 app.get("/urls/new", (req, res) => { //handles any get method to url of urls/new and renders the file in views/urls_new
   res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => { // handles request method of get and url of urls/:id
+  let templateVars = {
+    shortURL: req.params.id,
+    urls: urlDatabase,
+    username: req.cookies['username']
+   }; //I am also passing the object of our database to compare with the parameter that was given to us by the user
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+
+  var url= req.url.replace(/\/u\//i,"");
+  if (urlDatabase[url]){
+    res.redirect(urlDatabase[url]);
+  } else{
+    res.send('404 This page does not exist')
+  }
+
 });
 
 
@@ -51,31 +69,25 @@ app.post("/urls/:id/delete", (req,res) =>{ //this handles the post request to an
 });
 
 
-
-app.get("/urls/:id", (req, res) => { // handles request method of get and url of urls/:id
-  let templateVars = { shortURL: req.params.id, urls: urlDatabase }; //I am also passing the object of our database to compare with the parameter that was given to us by the user
-  res.render("urls_show", templateVars);
-});
-
-
 app.post("/urls/:id" , (req, res) => { //handles post method request on any urls/:query
   var updateurl= req.params.id;
   urlDatabase[updateurl] = req.body["newurl"] //changes the database
   res.redirect('/urls');
 });
 
-
-
-app.get("/u/:shortURL", (req, res) => {
-
-  var url= req.url.replace(/\/u\//i,"");
-  if (urlDatabase[url]){
-    res.redirect(urlDatabase[url]);
-  } else{
-    res.send('404 This page does not exist')
-  }
-
+app.post('/login', (req, res) => {
+  res.cookie('username',req.body['username']);
+  res.redirect('/urls');
 });
+
+app.post('/logout', (req, res) =>{
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
+
+
+
 
 app.listen(PORT, () => { //listens to port 8080
   console.log(`Example app listening on port ${PORT}!`);
