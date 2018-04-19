219130ae663198e6e13a5aa33c73ecc4dd7abb62
benyoo5222 <benyoo5222@gmail.com>
2018-04-19 19:01:12 +0000

    user registration, log in and logout

--- express_server.js
+++ express_server.js
@@ -2,27 +2,46 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 var cookieParser = require('cookie-parser'); // using the cookieparser
-
-
 const bodyParser = require("body-parser");
+
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.set("view engine", "ejs"); // telling the express library to run ejs(embedded javascript) templates to run in a folder called views
 
+//Start of databases
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-/*app.get("/", (req, res) => { //handles any get method with the url of /
-  let templateVars = { urls: urlDatabase };
+const users = {
+  user1: {
+    id: 'beny',
+    email: 'benyoo@gmail.com',
+    password: "purple-monkey-dinosaur"
+  },
+ user2: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+
+}
+
+
+// Start of get requests routers
+
+app.get("/", (req, res) => { //handles any get method with the url of /
+  let templateVars = { urls: urlDatabase, cookieid: cookieidchecker(req) };
   res.render("urls_index", templateVars);
-});*/
+})
 
 app.get("/urls", (req, res) => { //handles any get method to url of /urls and renders the urls_index page & the object of the urldatabase
-  let templateVars = { urls: urlDatabase, username: req.cookies['username']};
+  let templateVars = { urls: urlDatabase, cookieid: cookieidchecker(req)};
   res.render("urls_index", templateVars);
 });
 
@@ -35,7 +54,7 @@ app.get("/urls/:id", (req, res) => { // handles request method of get and url of
   let templateVars = {
     shortURL: req.params.id,
     urls: urlDatabase,
-    username: req.cookies['username']
+    cookieid: cookieidchecker(req)
    }; //I am also passing the object of our database to compare with the parameter that was given to us by the user
   res.render("urls_show", templateVars);
 });
@@ -51,6 +70,13 @@ app.get("/u/:shortURL", (req, res) => {
 
 });
 
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/login', (req, res) =>{
+  res.render('login'); // renders the ejs file called login
+});
 
 
 //Start of POST routers
@@ -75,18 +101,44 @@ app.post("/urls/:id" , (req, res) => { //handles post method request on any urls
   res.redirect('/urls');
 });
 
-app.post('/login', (req, res) => {
-  res.cookie('username',req.body['username']);
-  res.redirect('/urls');
-});
 
 app.post('/logout', (req, res) =>{
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
+app.post('/register', (req, res) => {
 
+  if(req.body['email'] && req.body['password']){
 
+    for(var key in users){ //gets the keys for person in the database
+      if(users[key].email == req.body['email']){ // if there is an email in the database already
+        res.send('This email is already taken please use another email'); //im telling the user to register another one
+      } else {
+        var randomid= generateRandomString(); //generates a random id so that i can assign it to a varibale so it points to one value;
+
+        users[randomid]={ //created a new key and object within the database object
+        id: randomid,
+        email: req.body['email'],
+        password: req.body['password']
+        };
+      }
+    }
+    res.cookie('user_id', users[randomid].id); //once all the checks are done, I send the user a cookie with their unique user id
+    res.redirect('/'); //once its successful it redirects to the main page
+
+  } else if(!req.body['email']){
+    res.send('404 Please write a valid email.');
+  } else if(!req.body['password']){
+    res.send('404 Please fill in a password.');
+  }
+
+});
+
+app.post('/login', (req, res) => {
+
+  emailpasswordchecker(req,res);
+});
 
 
 app.listen(PORT, () => { //listens to port 8080
@@ -116,3 +168,28 @@ function addhttp(newurl,longURL) {
     return urlDatabase;
   }
 }
+
+function cookieidchecker(req){
+
+  for(var key in users){
+    if(key == req.cookies['user_id']){
+      return users[key];
+    }
+  }
+}
+
+function emailpasswordchecker(req, res){
+
+  for(var key in users){
+      if(users[key].email == req.body['email'] && users[key].password == req.body['password']) {
+        res.cookie('user_id', users[key].id);
+        res.redirect('/urls');
+
+      } else if(users[key].email == req.body['email'] && users[key].password !== req.body['password']) {
+        res.send('403 Wrong password');
+      }
+  }
+  res.send('403 That email does not exist');
+}
+
+
