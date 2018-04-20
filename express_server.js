4835701edc8e80c3e0f1d47b71ef8477a264dffc
benyoo5222 <benyoo5222@gmail.com>
2018-04-20 01:02:38 +0000

    Made sure its working properly

--- express_server.js
+++ express_server.js
@@ -14,8 +14,17 @@ app.set("view engine", "ejs"); // telling the express library to run ejs(embedde
 //Start of databases
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2":
+  {
+    website:"http://www.lighthouselabs.ca",
+    userid: "blah"
+
+  },
+  "9sm5xK":
+  {
+    website: "http://www.google.com",
+    userid: "blah2"
+  }
 };
 
 const users = {
@@ -35,6 +44,14 @@ const users = {
 
 // Start of get requests routers
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/users.json", (req, res) => {
+  res.json(users);
+});
+
 app.get("/", (req, res) => { //handles any get method with the url of /
   let templateVars = { urls: urlDatabase, cookieid: cookieidchecker(req) };
   res.render("urls_index", templateVars);
@@ -47,13 +64,19 @@ app.get("/urls", (req, res) => { //handles any get method to url of /urls and re
 
 
 app.get("/urls/new", (req, res) => { //handles any get method to url of urls/new and renders the file in views/urls_new
-  res.render("urls_new");
+
+  if(cookieidchecker(req)){
+    res.render("urls_new");
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.get("/urls/:id", (req, res) => { // handles request method of get and url of urls/:id
   let templateVars = {
     shortURL: req.params.id,
     urls: urlDatabase,
+    web: urlDatabase[req.params.id],
     cookieid: cookieidchecker(req)
    }; //I am also passing the object of our database to compare with the parameter that was given to us by the user
   res.render("urls_show", templateVars);
@@ -63,7 +86,7 @@ app.get("/u/:shortURL", (req, res) => {
 
   var url= req.url.replace(/\/u\//i,"");
   if (urlDatabase[url]){
-    res.redirect(urlDatabase[url]);
+    res.redirect(urlDatabase[url].website);
   } else{
     res.send('404 This page does not exist')
   }
@@ -82,23 +105,33 @@ app.get('/login', (req, res) =>{
 //Start of POST routers
 app.post("/urls", (req, res) => { //Once there is a POST or have values in the body of the request to change the page, this gets invoked for /urls
   var newurl = generateRandomString(); //req.body is an object that has the key of longURL thats assigned by <input> longURL= {longURL:"given URL by user"}
-  var newDatabase=addhttp(newurl, req.body);
+  var newDatabase=addhttp(newurl, req.body, req.cookies);
+
   res.redirect(`urls/${newurl}`);
 });
 
 
 
 app.post("/urls/:id/delete", (req,res) =>{ //this handles the post request to any urls/query of the shortened url/delete
+
+  if(req.cookies['user_id'] && req.cookies['user_id'] == urlDatabase[req.body['deleteurl']].userID){
   var gettingshorturl = req.body["deleteurl"];
   delete urlDatabase[gettingshorturl];
   res.redirect('/urls');
+  }
+  res.send('You cannot delete other URLS');
+
+
 });
 
 
 app.post("/urls/:id" , (req, res) => { //handles post method request on any urls/:query
+  if(req.cookies['user_id'] && req.cookies['user_id'] == urlDatabase[req.params.id].userID){
   var updateurl= req.params.id;
-  urlDatabase[updateurl] = req.body["newurl"] //changes the database
+  urlDatabase[updateurl].website = req.body["newurl"] //changes the database
   res.redirect('/urls');
+  }
+  res.send('This is not your URL!');
 });
 
 
@@ -109,12 +142,14 @@ app.post('/logout', (req, res) =>{
 
 app.post('/register', (req, res) => {
 
-  if(req.body['email'] && req.body['password']){
+
+  /*if(req.body['email'] && req.body['password']){
 
     for(var key in users){ //gets the keys for person in the database
       if(users[key].email == req.body['email']){ // if there is an email in the database already
         res.send('This email is already taken please use another email'); //im telling the user to register another one
-      } else {
+      } else{
+      if(users[key].email != req.body['email']){
         var randomid= generateRandomString(); //generates a random id so that i can assign it to a varibale so it points to one value;
 
         users[randomid]={ //created a new key and object within the database object
@@ -122,22 +157,54 @@ app.post('/register', (req, res) => {
         email: req.body['email'],
         password: req.body['password']
         };
+
+        }
       }
     }
-    res.cookie('user_id', users[randomid].id); //once all the checks are done, I send the user a cookie with their unique user id
-    res.redirect('/'); //once its successful it redirects to the main page
+    res.cookie('user_id', users[randomid].id);
+    res.redirect('/');
+     //once all the checks are done, I send the user a cookie with their unique user id
+     //once its successful it redirects to the main page
 
   } else if(!req.body['email']){
     res.send('404 Please write a valid email.');
   } else if(!req.body['password']){
     res.send('404 Please fill in a password.');
+  }*/
+
+  var randomid= generateRandomString();
+
+  if(req.body['email'] && req.body['password']){
+    for(var key in users){
+      if(users[key].email == req.body['email']){
+        res.send('404 existing email');
+      }
+    }
+
+    users[randomid]={ //created a new key and object within the database object
+        id: randomid,
+        email: req.body['email'],
+        password: req.body['password']
+        };
+        res.cookie('user_id', users[randomid].id);
+        res.redirect('/');
+     //generates a random id so that i can assign it to a varibale so it points to one value
+  } else {
+    res.send('404 please write valid email and password');
   }
 
 });
 
 app.post('/login', (req, res) => {
 
-  emailpasswordchecker(req,res);
+  for(var key in users){
+   if(users[key].email == req.body['email'] && users[key].password == req.body['password']) {
+    res.cookie('user_id', users[key].id);
+    res.redirect('/urls');
+    return ;
+    }
+  }
+    res.send('403 no match');
 });
 
 
@@ -158,13 +225,20 @@ function generateRandomString() {
 
 }
 
-function addhttp(newurl,longURL) {
+function addhttp(newurl,longURL,cookies) {
+
 
   if (longURL["longURL"].startsWith('http')){
-    urlDatabase[newurl] = longURL["longURL"]; //This adds a new key to the database
+    urlDatabase[newurl] = {};
+    urlDatabase[newurl].website = longURL["longURL"]; //This adds a new key to the database
+    urlDatabase[newurl].userID = cookies['user_id'];
+
     return urlDatabase; //returns the newdatabase
   } else {
-    urlDatabase[newurl] = 'http://'+longURL["longURL"];
+    urlDatabase[newurl] = {};
+    urlDatabase[newurl].website = 'http://'+longURL["longURL"];
+    urlDatabase[newurl].userID = cookies['user_id'];
+
     return urlDatabase;
   }
 }
@@ -178,18 +252,18 @@ function cookieidchecker(req){
   }
 }
 
-function emailpasswordchecker(req, res){
+/*function emailpasswordchecker(req, res,){
 
   for(var key in users){
-      if(users[key].email == req.body['email'] && users[key].password == req.body['password']) {
-        res.cookie('user_id', users[key].id);
-        res.redirect('/urls');
-
-      } else if(users[key].email == req.body['email'] && users[key].password !== req.body['password']) {
-        res.send('403 Wrong password');
-      }
+   if(users[key].email == req.body['email'] && users[key].password == req.body['password']) {
+    res.cookie('user_id', users[key].id);
+    res.redirect('/urls');
+    res.end();
+    }
   }
-  res.send('403 That email does not exist');
-}
+    res.send('403 no match');
+
+
+}*/
 
 
